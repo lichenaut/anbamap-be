b5cc2e2ee3f3f531e2c5ec36740a74e36966fd1f
lichenaut <mt0n@tutanota.com>
2024-05-10 13:32:05 -0600

    transition to more structured region vec building

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod scrape {
         pub mod youtube;
     }
     pub mod scraper_util;
+    mod region;
 }
 use scrape::scrapers::youtube::scrape_youtube_channel;
 use std::{error::Error/*, process::Command*/};
@@ -11,7 +12,7 @@ use std::{error::Error/*, process::Command*/};
 async fn main() -> Result<(), Box<dyn Error>> {
     tracing_subscriber::fmt().with_max_level(tracing::Level::TRACE).init();
     //let update = Command::new("bash").arg("-c").arg("source /home/lichenaut/p3env/bin/activate && pip install flashgeotext").output()?;
-    scrape_youtube_channel("UC8p1vwvWtl6T73JiExfWs1g").await?;
+    //scrape_youtube_channel("UC8p1vwvWtl6T73JiExfWs1g").await?;
 
     Ok(())
 }
