ad4f812f99fe6a16c01d27cf7afa1c80897086c8
lichenaut <mt0n@tutanota.com>
2024-05-19 17:54:02 -0600

    keyphrase qc

--- src/db/keyphrase_db.rs
+++ src/db/keyphrase_db.rs
@@ -10,7 +10,6 @@ use crate::scrape::scrapers::forbes400::get_largest_billionaires_map;
 use crate::scrape::scrapers::wikidata::region_code_to_figures;
 use crate::scrape::scrapers::wikimedia::get_private_enterprises_map;
 
-
 struct Region {
     region_code: String,
     keyphrases: String,
@@ -53,97 +52,102 @@ pub async fn gen_keyphrase_db() -> Result<(), Box<dyn std::error::Error>> {
         )"
     ).await?;
     
-    // let all_countries_path = format!("{}/src/db/allCountries.txt", current_dir.display()); // https://download.geonames.org/export/dump/allCountries.zip
-    // let reader = io::BufReader::new(File::open(Path::new(&all_countries_path))?);
-    // for line in reader.lines() {
-    //     let line = line?;
-    //     let mut fields = line.split("\t");
-    //     let ascii_name = match fields.nth(2) {
-    //         Some(ascii_name) => ascii_name,
-    //         None => continue,
-    //     };
-    //     let feature_class = match fields.nth(3) {
-    //         Some(feature_class) => feature_class,
-    //         None => continue,
-    //     };
-    //     let feature_code = match fields.nth(0) {
-    //         Some(feature_code) => feature_code,
-    //         None => continue,
-    //     };
-    //     let region_code = match fields.nth(0) {
-    //         Some(region_code) => region_code,
-    //         None => continue,
-    //     };
-    //     let population = match fields.nth(5) {
-    //         Some(population) => {
-    //             match population.parse::<u32>() {
-    //                 Ok(population) => population,
-    //                 Err(_) => continue,
-    //             }
-    //         },
-    //         None => continue,
-    //     };
+    let all_countries_path = format!("{}/src/db/allCountries.txt", current_dir.display()); // https://download.geonames.org/export/dump/allCountries.zip
+    let reader = io::BufReader::new(File::open(Path::new(&all_countries_path))?);
+    for line in reader.lines() {
+        let line = line?;
+        let mut fields = line.split("\t");
+        let ascii_name = match fields.nth(2) {
+            Some(ascii_name) => ascii_name,
+            None => continue,
+        };
+        
+        let feature_class = match fields.nth(3) {
+            Some(feature_class) => feature_class,
+            None => continue,
+        };
+        
+        let feature_code = match fields.nth(0) {
+            Some(feature_code) => feature_code,
+            None => continue,
+        };
 
-    //     if feature_code.contains('H') { continue; }
+        let region_code = match fields.nth(0) {
+            Some(region_code) => region_code,
+            None => continue,
+        };
 
-    //     match feature_class {
-    //         "A" =>  {
-    //             if population < 490000 { continue; }
-    //         },
-    //         "P" => {
-    //             match feature_code {
-    //                 "PPLC" => {},
-    //                 _ => {
-    //                     if population < 290000 { continue; }
-    //                 },
-    //             }
-    //         },
-    //         _ => continue,
-    //     }
+        let population = match fields.nth(5) {
+            Some(population) => {
+                match population.parse::<u32>() {
+                    Ok(population) => population,
+                    Err(_) => continue,
+                }
+            },
+            None => continue,
+        };
 
-    //     update_region(&pool, Region {
-    //         region_code: region_code.to_string(),
-    //         keyphrases: format!("{}", ascii_name),
-    //     }).await?;
-    // }
+        if feature_code.contains('H') { continue; }
 
-    // let country_info_path = format!("{}/src/db/countryInfo.txt", current_dir.display()); // https://download.geonames.org/export/dump/countryInfo.txt
-    // let reader = io::BufReader::new(File::open(Path::new(&country_info_path))?);
-    // for line in reader.lines() {
-    //     let line = line?;
-    //     let mut fields = line.split("\t");
-    //     let region_code = match fields.nth(0) {
-    //         Some(region_code) => region_code,
-    //         None => continue,
-    //     };
-    //     let capital = match fields.nth(4) {
-    //         Some(capital) => capital,
-    //         None => continue,
-    //     };
+        match feature_class {
+            "A" =>  {
+                if population < 490000 { continue; }
+            },
+            "P" => {
+                match feature_code {
+                    "PPLC" => {},
+                    _ => {
+                        if population < 290000 { continue; }
+                    },
+                }
+            },
+            _ => continue,
+        }
 
-    //     update_region(&pool, Region {
-    //         region_code: region_code.to_string(),
-    //         keyphrases: format!("{}", capital),
-    //     }).await?;
-    // }
+        update_region(&pool, Region {
+            region_code: region_code.to_string(),
+            keyphrases: format!("{}", ascii_name),
+        }).await?;
+    }
+
+    let country_info_path = format!("{}/src/db/countryInfo.txt", current_dir.display()); // https://download.geonames.org/export/dump/countryInfo.txt
+    let reader = io::BufReader::new(File::open(Path::new(&country_info_path))?);
+    for line in reader.lines() {
+        let line = line?;
+        let mut fields = line.split("\t");
+        let region_code = match fields.nth(0) {
+            Some(region_code) => region_code,
+            None => continue,
+        };
+
+        let capital = match fields.nth(4) {
+            Some(capital) => capital,
+            None => continue,
+        };
+
+        update_region(&pool, Region {
+            region_code: region_code.to_string(),
+            keyphrases: format!("{}", capital),
+        }).await?;
+    }
 
     // Wikidata property verifying is only necessary when running for the first time or when it's been a long time since the previous verification.
     // crate::scrape::scrapers::wikidata::verify_codes().await;
     let region_codes = vec!["AD", "AE", "AF", "AG", "AI", "AL", "AM", "AO", "AQ", "AR", "AS", "AT", "AU", "AW", "AX", "AZ", "BA", "BB", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BL", "BM", "BN", "BO", "BQ", "BR", "BS", "BT", "BV", "BW", "BY", "BZ", "CA", "CC", "CD", "CF", "CG", "CH", "CI", "CK", "CL", "CM", "CN", "CO", "CR", "CU", "CV", "CW", "CX", "CY", "CZ", "DE", "DJ", "DK", "DM", "DO", "DZ", "EC", "EE", "EG", "EH", "ER", "ES", "ET", "FI", "FJ", "FK", "FM", "FO", "FR", "GA", "GB", "GD", "GE", "GF", "GG", "GH", "GI", "GL", "GM", "GN", "GP", "GQ", "GR", "GS", "GT", "GU", "GW", "GY", "HK", "HM", "HN", "HR", "HT", "HU", "ID", "IE", "IL", "IM", "IN", "IO", "IQ", "IR", "IS", "IT", "JE", "JM", "JO", "JP", "KE", "KG", "KH", "KI", "KM", "KN", "KP", "KR", "KW", "KY", "KZ", "LA", "LB", "LC", "LI", "LK", "LR", "LS", "LT", "LU", "LV", "LY", "MA", "MC", "MD", "ME", "MF", "MG", "MH", "MK", "ML", "MM", "MN", "MO", "MP", "MQ", "MR", "MS", "MT", "MU", "MV", "MW", "MX", "MY", "MZ", "NA", "NC", "NE", "NF", "NG", "NI", "NL", "NO", "NP", "NR", "NU", "NZ", "OM", "PA", "PE", "PF", "PG", "PH", "PK", "PL", "PM", "PN", "PR", "PS", "PT", "PW", "PY", "QA", "RE", "RO", "RS", "RU", "RW", "SA", "SB", "SC", "SD", "SE", "SG", "SH", "SI", "SJ", "SK", "SL", "SM", "SN", "SO", "SR", "SS", "ST", "SV", "SX", "SY", "SZ", "TC", "TD", "TF", "TG", "TH", "TJ", "TK", "TL", "TM", "TN", "TO", "TR", "TT", "TV", "TW", "TZ", "UA", "UG", "UM", "US", "UY", "UZ", "VA", "VC", "VE", "VG", "VI", "VN", "VU", "WF", "WS", "YE", "YT", "ZA", "ZM", "ZW"];
     let client = reqwest::Client::new();
-    // for region_code in &region_codes {
-    //     let heads_of_state = region_code_to_figures(&client, region_code).await?;
-    //     for head_of_state in heads_of_state {
-    //         update_region(&pool, Region {
-    //             region_code: region_code.to_string(),
-    //             keyphrases: head_of_state,
-    //         }).await?;
-    //     }
-    // }
-    
-    let largest_private_enterprises = get_private_enterprises_map(&client).await?;
-    for (calculated_code, largest_private_enterprises) in largest_private_enterprises {
-        for keyphrase in largest_private_enterprises {
+    for region_code in &region_codes {
+        let heads_of_state = region_code_to_figures(&client, region_code).await?;
+        for head_of_state in heads_of_state {
+            update_region(&pool, Region {
+                region_code: region_code.to_string(),
+                keyphrases: head_of_state,
+            }).await?;
+        }
+    }
+
+    let largest_billionaires = get_largest_billionaires_map(&client).await?;
+    for (calculated_code, billionaires) in largest_billionaires {
+        for keyphrase in billionaires {
             update_region(&pool, Region {
                 region_code: calculated_code.to_string(),
                 keyphrases: keyphrase,
@@ -151,9 +155,9 @@ pub async fn gen_keyphrase_db() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
     
-    let largest_billionaires = get_largest_billionaires_map(&client).await?;
-    for (calculated_code, billionaires) in largest_billionaires {
-        for keyphrase in billionaires {
+    let largest_private_enterprises = get_private_enterprises_map(&client).await?;
+    for (calculated_code, largest_private_enterprises) in largest_private_enterprises {
+        for keyphrase in largest_private_enterprises {
             update_region(&pool, Region {
                 region_code: calculated_code.to_string(),
                 keyphrases: keyphrase,
