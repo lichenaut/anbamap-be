f4e5a7b89a08f27ac9ddb6dc0b8e717919e3d612
lichenaut <mt0n@tutanota.com>
2024-05-29 21:03:15 -0600

    zip_service.rs refactor

--- src/util/zip_service.rs
+++ src/util/zip_service.rs
@@ -9,27 +9,24 @@ use tokio::{
 use tokio_stream::StreamExt;
 
 pub async fn zip_from_url(client: &Client, url: &str, zip_path: &str) -> Result<()> {
-    let response = client.get(url).send().await?;
-    let mut stream = response.bytes_stream();
-    let file = File::create(zip_path).await?;
-    let mut zip_file = BufWriter::new(file);
+    let mut zip_file = BufWriter::new(File::create(zip_path).await?);
+    let mut stream = client.get(url).send().await?.bytes_stream();
 
     while let Some(item) = stream.next().await {
-        let item = item?;
-        zip_file.write_all(&item).await?;
+        zip_file.write_all(&item?).await?;
     }
-
     zip_file.flush().await?;
 
     Ok(())
 }
 
 pub async fn zip_to_txt(zip_path: &str) -> Result<()> {
-    let mut file = BufReader::new(File::open(zip_path).await?);
-    let mut zip = ZipFileReader::with_tokio(&mut file).await?;
-    let mut reader = zip.reader_with_entry(0).await?;
+    let mut zip_reader = BufReader::new(File::open(zip_path).await?);
+    let mut zip_reader = ZipFileReader::with_tokio(&mut zip_reader).await?;
+    let mut zip_reader = zip_reader.reader_with_entry(0).await?;
+
     let mut buffer = Vec::new();
-    reader.read_to_end(&mut buffer).await?;
+    zip_reader.read_to_end(&mut buffer).await?;
     let mut file =
         BufWriter::new(File::create(format!("{}.txt", &zip_path[..zip_path.len() - 4])).await?);
     file.write_all(&buffer).await?;
